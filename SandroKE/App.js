c49d94e9c6329bdb9e83415ed6d83b76a49a2792
SandroKE <koomealessandro@gmail.com>
2024-01-12 10:55:08 -0700

    intial commit

--- SandroKE/App.js
+++ SandroKE/App.js
@@ -3,18 +3,29 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
+    <>
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world</Text>
+      <View style={styles.dead}></View>
       <StatusBar style="auto" />
     </View>
+   
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  dead: {
+    height:'20%',
+    width:'50%',
+    backgroundColor:'aliceblue'
+  }
+  
 });
